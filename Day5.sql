43bc4a6b2c2a310bd549eb2f55b5a8fbfc6aa3a4
verma8986 <35268279+verma8986@users.noreply.github.com>
2018-01-12 11:17:13 -0500

    Add files via upload

---
+++ Day5.sql
@@ -0,0 +1,133 @@
+-- MySQL dump 10.16  Distrib 10.1.21-MariaDB, for Win32 (AMD64)
+--
+-- Host: localhost    Database: localhost
+-- ------------------------------------------------------
+-- Server version	10.1.21-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `c1`
+--
+
+DROP TABLE IF EXISTS `c1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `c1` (
+  `ID` int(3) NOT NULL,
+  `LastName` varchar(255) NOT NULL,
+  `FirstName` varchar(255) DEFAULT NULL,
+  `Age` int(3) DEFAULT NULL,
+  `cityID` int(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `c1`
+--
+
+LOCK TABLES `c1` WRITE;
+/*!40000 ALTER TABLE `c1` DISABLE KEYS */;
+/*!40000 ALTER TABLE `c1` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `c2`
+--
+
+DROP TABLE IF EXISTS `c2`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `c2` (
+  `ID` int(3) NOT NULL,
+  `LastName` varchar(255) NOT NULL,
+  `FirstName` varchar(255) DEFAULT NULL,
+  `Age` int(3) DEFAULT NULL,
+  `cityID` int(4) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `fk_persons` (`cityID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `c2`
+--
+
+LOCK TABLES `c2` WRITE;
+/*!40000 ALTER TABLE `c2` DISABLE KEYS */;
+INSERT INTO `c2` VALUES (1,'verma','Sonal',23,1),(2,'Macwan','Priya',30,2),(3,'parmar','lizza',15,1);
+/*!40000 ALTER TABLE `c2` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `citymaster`
+--
+
+DROP TABLE IF EXISTS `citymaster`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `citymaster` (
+  `CityID` int(4) NOT NULL,
+  `cityName` varchar(255) DEFAULT 'toronto',
+  PRIMARY KEY (`CityID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `citymaster`
+--
+
+LOCK TABLES `citymaster` WRITE;
+/*!40000 ALTER TABLE `citymaster` DISABLE KEYS */;
+INSERT INTO `citymaster` VALUES (1,'London');
+/*!40000 ALTER TABLE `citymaster` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `persons`
+--
+
+DROP TABLE IF EXISTS `persons`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `persons` (
+  `ID` int(3) NOT NULL,
+  `LastName` varchar(255) NOT NULL,
+  `FirstName` varchar(255) DEFAULT NULL,
+  `Age` int(3) DEFAULT NULL,
+  `cityID` int(4) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `fk_persons` (`cityID`),
+  CONSTRAINT `fk_persons` FOREIGN KEY (`cityID`) REFERENCES `citymaster` (`CityID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `persons`
+--
+
+LOCK TABLES `persons` WRITE;
+/*!40000 ALTER TABLE `persons` DISABLE KEYS */;
+INSERT INTO `persons` VALUES (0,'',NULL,NULL,1),(1,'verma','Sonal',23,1),(3,'parmar','lizza',15,1);
+/*!40000 ALTER TABLE `persons` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-01-12 10:55:49
